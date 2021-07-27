e5c9193f976bdd1d97148c0c2d9cf510627cd732
Marcelo Fanticelli <marcelo.fanticelli@knowledge-itsolutions.com>
2021-07-27 09:09:28 -0300

    Remove unused objects

--- Sales.CrossCutting.IoC/DependencyInjection/RegisterServices.cs
+++ Sales.CrossCutting.IoC/DependencyInjection/RegisterServices.cs
@@ -22,10 +22,8 @@ namespace Sales.CrossCutting.IoC.DependencyInjection
         public static void RegisterAppServices(this IServiceCollection services)
         {
             services.AddScoped<ICategoryAppService, CategoryAppService>();
-            services.AddScoped<IItemCategoryAppService, ItemCategoryAppService>();
             services.AddScoped<IItemPriceAppService, ItemPriceAppService>();
             services.AddScoped<IItemAppService, ItemAppService>();
-            services.AddScoped<IOrderItemAppService, OrderItemAppService>();
             services.AddScoped<IOrderAppService, OrderAppService>();
             services.AddScoped<ITaxAppService, TaxAppService>();
         }
@@ -33,10 +31,8 @@ namespace Sales.CrossCutting.IoC.DependencyInjection
         public static void RegisterDomainServices(this IServiceCollection services)
         {
             services.AddScoped<ICategoryService, CategoryService>();
-            services.AddScoped<IItemCategoryService, ItemCategoryService>();
             services.AddScoped<IItemPriceService, ItemPriceService>();
             services.AddScoped<IItemService, ItemService>();
-            services.AddScoped<IOrderItemService, OrderItemService>();
             services.AddScoped<IOrderService, OrderService>();
             services.AddScoped<ITaxService, TaxService>();
         }
@@ -44,10 +40,8 @@ namespace Sales.CrossCutting.IoC.DependencyInjection
         public static void RegisterRepositories(this IServiceCollection services)
         {
             services.AddScoped<ICategoryRepository, CategoryRepository>();
-            services.AddScoped<IItemCategoryRepository, ItemCategoryRepository>();
             services.AddScoped<IItemPriceRepository, ItemPriceRepository>();
             services.AddScoped<IItemRepository, ItemRepository>();
-            services.AddScoped<IOrderItemRepository, OrderItemRepository>();
             services.AddScoped<IOrderRepository, OrderRepository>();
             services.AddScoped<ITaxRepository, TaxRepository>();
         }
